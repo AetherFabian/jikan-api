082f49b513accaf4fb68773ab88cbc110075c228
Fabian Escobar <50721208+AetherFabian@users.noreply.github.com>
2022-03-07 04:09:42 -0700

    Deploy Testing 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import "../assets/css/main.css"
 function Header() {
 	return (
 <header>
-    	<img src="https://avatars.githubusercontent.com/u/30051078?s=280&v=4" alt="image of header"/>
+    	<img src="https://avatars.githubusercontent.com/u/30051078?s=280&v=4" alt="just header"/>
 </header>
 	)
 }
